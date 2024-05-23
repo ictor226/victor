f70cd213d67837d41ccfd72b2210bb8e8fc64c93
ictor226 <167533745+ictor226@users.noreply.github.com>
2024-05-23 12:01:07 -0300

    aula sobre operadores logicos

---
+++ java.js
@@ -0,0 +1,110 @@
+var tabuada = 4 
+
+var multiplicacao1 = tabuada * 1  
+var multiplicacao2 = tabuada * 2  
+var multiplicacao3 = tabuada * 3  
+var multiplicacao4 = tabuada * 4
+var multiplicacao5 = tabuada * 5  
+var multiplicacao6 = tabuada * 6 
+var multiplicacao7 = tabuada * 7  
+var multiplicacao8 = tabuada * 8  
+var multiplicacao9 = tabuada * 9  
+var multiplicacao10 = tabuada * 10
+
+console.log(multiplicacao1)
+console.log(multiplicacao2)
+console.log(multiplicacao3)
+console.log(multiplicacao4)
+console.log(multiplicacao5)
+console.log(multiplicacao6)
+console.log(multiplicacao7)
+console.log(multiplicacao8)
+console.log(multiplicacao9)
+console.log(multiplicacao10)
+
+Atribuição
+ 
+Adição
+ 
+Subtração
+ 
+1-
+ 
+• Divisão
+ 
+1+
+ 
+17
+ 
+Multiplicação.
+ 
+Negação
+ 
+Ifalse
+ 
+* Igual
+ 
+33
+ 
+| true
+ 
+* Diferente
+ 
+3!
+ 
+3.1 |
+ 
+true
+ 
+* Maior
+ 
+144
+ 
+| false
+ 
+* Menor
+ 
+|
+ 
+|<
+ 
+144
+ 
+| false
+ 
+* Maior Igual | >=
+ 
+44
+ 
+true
+ 
+* Menor Igual |
+ 
+<=
+ 
+155
+ 
+| true
+ 
+| true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
