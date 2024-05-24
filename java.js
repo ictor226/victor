443861eb0a8cd1b1a0c1e0f50ffdf7120882a583
ictor226 <167533745+ictor226@users.noreply.github.com>
2024-05-24 09:21:32 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,110 +1,31 @@
-var tabuada = 4 
+//atividade 01
+var numero1 = 3
+var numero2 = 3
+console.log (numero1 + numero2)
+console.log (numero1 - numero2)
+console.log (numero1 * numero2)
+console.log (numero1 / numero2)
 
-var multiplicacao1 = tabuada * 1  
-var multiplicacao2 = tabuada * 2  
-var multiplicacao3 = tabuada * 3  
-var multiplicacao4 = tabuada * 4
-var multiplicacao5 = tabuada * 5  
-var multiplicacao6 = tabuada * 6 
-var multiplicacao7 = tabuada * 7  
-var multiplicacao8 = tabuada * 8  
-var multiplicacao9 = tabuada * 9  
-var multiplicacao10 = tabuada * 10
+//atividade 02
+var celsius= ((126 - 32)/ 1.8)
+console.log ("C° =",celsius)
 
-console.log(multiplicacao1)
-console.log(multiplicacao2)
-console.log(multiplicacao3)
-console.log(multiplicacao4)
-console.log(multiplicacao5)
-console.log(multiplicacao6)
-console.log(multiplicacao7)
-console.log(multiplicacao8)
-console.log(multiplicacao9)
-console.log(multiplicacao10)
+//atividade03
+var f=  (77 * (9 / 5 ) + 32)
+console.log("F° = ", f)
+//abreviei por ser um atalho
 
-Atribuição
- 
-Adição
- 
-Subtração
- 
-1-
- 
-• Divisão
- 
-1+
- 
-17
- 
-Multiplicação.
- 
-Negação
- 
-Ifalse
- 
-* Igual
- 
-33
- 
-| true
- 
-* Diferente
- 
-3!
- 
-3.1 |
- 
-true
- 
-* Maior
- 
-144
- 
-| false
- 
-* Menor
- 
-|
- 
-|<
- 
-144
- 
-| false
- 
-* Maior Igual | >=
- 
-44
- 
-true
- 
-* Menor Igual |
- 
-<=
- 
-155
- 
-| true
- 
-| true
+//ATIVIDADE 04
+var m = (7 * 200)
+//m seria metros
+// cm seria cemtimetros 
+console.log ("CM = ", m)
 
+//ATIVIDADE05
+var p = 100
+var d = (p * 0.9)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log (`R$${p}`)
+console.log (`R$${d}`)
+//p seria preço
+//d seria desconto
